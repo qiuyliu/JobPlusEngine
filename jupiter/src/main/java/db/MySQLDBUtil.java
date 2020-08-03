933bf7d2f7727cc6ea79070bd3662c1cee41ce5c
qiuyaoliu <ql459@nyu.edu>
2020-08-02 17:47:27 -0700

    update default

--- jupiter/src/main/java/db/MySQLDBUtil.java
+++ jupiter/src/main/java/db/MySQLDBUtil.java
@@ -1,11 +1,11 @@
 package db;
 
 public class MySQLDBUtil {
-	private static final String INSTANCE = "laiproject.cccxoxizmq9e.us-east-2.rds.amazonaws.com";
-	private static final String PORT_NUM = "3306";
+	private static final String INSTANCE = "YOUR_INSTANCE";
+	private static final String PORT_NUM = "YOUR_PORT";
 	public static final String DB_NAME = "jupiter";
-	private static final String USERNAME = "admin";
-	private static final String PASSWORD = "lqyLQY123";
+	private static final String USERNAME = "YOUR_USER_NAME";
+	private static final String PASSWORD = "YOUR_USER_PASSWORD";
 	// MySQL address on AWS
 	public static final String URL = "jdbc:mysql://"
 			+ INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
